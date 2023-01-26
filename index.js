3090c1cb6e7046ef1de4a8fb23208e71735c7756
Mingyu Cui <mingyu.cui@robinhood.com>
2023-01-25 16:31:09 -0800

    merge ok

--- index.js
+++ index.js
@@ -9,13 +9,34 @@ module.exports = (opts = {}) => {
 
     Once(root) {
       // split selectors by comma
+      // .a, .b {} -> .a {} .b {}
       root.walk((node) => {
-        if (!!node.selectors && node.selectors.length > 1) {
+        if (node.selectors && node.selectors.length > 1) {
           node.selectors.reverse().forEach((selector) => {
             node.cloneAfter({ selector }); // cloneAfter to recurse
           });
 
           node.remove();
+        }
+      });
+
+      /*
+                 root             root
+              a1      a2    ->     a
+            b1  b2  c1  c2        b c
+      */
+      // merge same selectors
+      root.walk((node) => {
+        node.raws.semicolon = true; // postcss strips final semicolon by default
+        if (node.selector) {
+          let next = node;
+          while ((next = next.next())) {
+            if (next.selector === node.selector) {
+              node.append(...next.nodes);
+
+              next.remove();
+            }
+          }
         }
       });
     },
--- index.test.js
+++ index.test.js
@@ -10,35 +10,53 @@ async function run(input, output, opts = {}) {
   expect(result.warnings()).toHaveLength(0);
 }
 
-it("does something", async () => {
+it("works", async () => {
   await run(
     `
-.a, .b {
-  p1: v1;
-  .c, .d {
-    p2: v2;
-  }
-}
+      .a {
+        p0: v0;
+      }
+      .a, .b {
+        .d {
+          p1: v1;
+        }
+        p2: v2;
+        .c, .d {
+          p3: v3;
+        }
+        .c {
+          p4: v4;
+        }
+      }
+      .b {
+        p5: v5;
+      }
     `,
     `
-.a {
-  p1: v1;
-  .c {
-    p2: v2;
-  }
-  .d {
-    p2: v2;
-  }
-}
-.b {
-  p1: v1;
-  .c {
-    p2: v2;
-  }
-  .d {
-    p2: v2;
-  }
-}
+      .a {
+        p0: v0;
+        .d {
+          p1: v1;
+          p3: v3;
+        }
+        p2: v2;
+        .c {
+          p3: v3;
+          p4: v4;
+        }
+      }
+      .b {
+        .d {
+          p1: v1;
+          p3: v3;
+        }
+        p2: v2;
+        .c {
+          p3: v3;
+          p4: v4;
+        }
+        p5: v5;
+      }
     `,
     {}
   );
