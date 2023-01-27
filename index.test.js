3fd9d2105ea7a99c6dd8c571c099e7680b02ab0c
Mingyu Cui <mingyu.cui@robinhood.com>
2023-01-26 22:47:09 -0800

    v0.0.1 merge multiple siblings

--- index.test.js
+++ index.test.js
@@ -10,7 +10,7 @@ async function run(input, output, opts = {}) {
   expect(result.warnings()).toHaveLength(0);
 }
 
-it("works", async () => {
+it("works with nested selectors", async () => {
   await run(
     `
       .a {
@@ -56,6 +56,80 @@ it("works", async () => {
           p4: v4;
         }
         p5: v5;
+      }
+    `,
+    {}
+  );
+});
+
+it("works with multiple same siblings", async () => {
+  await run(
+    `
+      .a {
+        p0: v0;
+      }
+      .b {
+        p1: v1;
+      }
+      .a {
+        p2: v2;
+      }
+      .b {
+        p3: v3;
+      }
+      .a {
+        p4: v4;
+      }
+      .b {
+        p5: v5;
+      }
+    `,
+    `
+      .a {
+        p0: v0;
+        p2: v2;
+        p4: v4;
+      }
+      .b {
+        p1: v1;
+        p3: v3;
+        p5: v5;
+      }
+    `,
+    {}
+  );
+});
+
+it("works on nested classes recursively", async () => {
+  await run(
+    `
+      .a {
+        .b {
+          p0: v0;
+        }
+        .c {
+          p1: v1;
+        }
+      }
+      .a {
+        .b {
+          p2: v2;
+        }
+        .c {
+          p3: v3;
+        }
+      }
+    `,
+    `
+      .a {
+        .b {
+          p0: v0;
+          p2: v2;
+        }
+        .c {
+          p1: v1;
+          p3: v3;
+        }
       }
     `,
     {}
